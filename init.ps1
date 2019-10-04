7be2d25bcb0df2f2564e0ad5b57f5a580f6fe53a
Sebastian Krysmanski <noreply@manski.net>
2019-10-04 07:59:19 +0200

    Improved output of init.ps1

--- init.ps1
+++ init.ps1
@@ -1,18 +1,20 @@
 #!/usr/bin/env pwsh
 
-Write-Host 'Removing ".git"...'
+Write-Host -ForegroundColor Cyan 'Removing ".git"...'
 Remove-Item "$PSScriptRoot/.git" -Recurse -Force
 
-Write-Host 'Removing "README.md"...'
+Write-Host -ForegroundColor Cyan 'Removing "README.md"...'
 Remove-Item "$PSScriptRoot/README.md"
 
-Write-Host 'Removing "init.ps1"...'
+Write-Host -ForegroundColor Cyan 'Removing "init.ps1"...'
 Remove-Item "$PSScriptRoot/init.ps1"
 
-Write-Host 'Initializing new Git repository...'
+Write-Host
+Write-Host -ForegroundColor Cyan 'Initializing new Git repository...'
 & git init "$PSScriptRoot"
 
-Write-Host 'Adding skeleton files...'
+Write-Host
+Write-Host -ForegroundColor Cyan 'Adding skeleton files...'
 & git add *
 & git commit -m 'Added repository skeleton'
 
